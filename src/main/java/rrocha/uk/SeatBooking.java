24be8f372c11a3059f306894e843f479f6875607
Ricardo Rocha <me@rrocha.uk>
2020-12-01 12:49:31 +0000

    Initial commit

---
+++ src/main/java/rrocha/uk/SeatBooking.java
@@ -0,0 +1,19 @@
+package rrocha.uk;
+
+/**
+ * Hello world!
+ */
+public final class SeatBooking {
+
+    static SeatPlan seats = new SeatPlan();
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+
+        seats.getSeatPlan();
+
+    }
+}
